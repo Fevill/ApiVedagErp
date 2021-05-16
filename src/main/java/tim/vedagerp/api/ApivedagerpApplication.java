b804f1144d0d80923305c05a5b2c94ccbb8ae94f
TIM VEN <tim.ven.mail@gmail.com>
2021-05-16 20:50:30 +0200

    initialisation du projet spring

---
+++ src/main/java/tim/vedagerp/api/ApivedagerpApplication.java
@@ -0,0 +1,13 @@
+package tim.vedagerp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivedagerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApivedagerpApplication.class, args);
+	}
+
+}
